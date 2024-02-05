ad4f1e7eea59226a0273343863d1d4d1322c36da
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-05 15:35:23 +1000

    added page

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import flet as ft
+from flet import Column, Row, Container, TextField, Checkbox
+from flet_core.control import Control
 
+
+def main(page: ft.Page) -> None:
+    page.title = 'Yoshino'
+
+
+if __name__ == '__main__':
+    ft.app(target=main,
+           assets_dir='assets'
+           )
