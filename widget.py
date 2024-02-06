e795ecc074aaf01d629ca15f8e99079c3b3131da
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-06 14:17:30 +1000

    pre-pre-pre-alpha version of widget

---
+++ widget.py
@@ -0,0 +1,61 @@
+import flet as ft
+from flet import Column, Row, Container, TextField, Checkbox
+from flet_core.control import Control
+
+
+def main(page: ft.Page) -> None:
+    page.title = 'Yoshino'
+    page.window_focused = True
+    page.theme_mode = 'dark'
+
+    page.window_maximizable = False
+    page.window_minimizable = False
+    page.window_resizable = False
+
+    page.window_title_bar_hidden = True
+    page.window_title_bar_buttons_hidden = False
+    page.window_frameless = True
+
+    page.window_height = 400
+    page.window_width = 400
+
+    page.window_center()
+
+    page.window_always_on_top = True
+    # page.window_to_front()
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+
+    c = ft.Container(
+        width=page.window_width,
+        height=page.window_height,
+        opacity=100
+
+    )
+
+    chest = ft.Stack(
+        controls=[
+            ft.Image(
+                src="assets/yoshino_chibi.png"
+            ),
+            c
+        ]
+
+    )
+
+    drag_area = ft.WindowDragArea(
+        content=chest,
+        maximizable=False
+    )
+
+    page.add(
+        drag_area
+    )
+    page.update()
+
+
+if __name__ == '__main__':
+    ft.app(target=main,
+           assets_dir='assets'
+           )
