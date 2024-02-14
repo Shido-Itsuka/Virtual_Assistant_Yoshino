06fa0cbc37b1b5f3d2a5c0718ec2e27106df5888
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-14 15:40:34 +1000

    nothing special

--- settings.py
+++ settings.py
@@ -5,8 +5,9 @@ from flet_core.control import Control
 
 def main(page: ft.Page) -> None:
     page.title = 'Settings'
-    page.window_focused = True
     page.theme_mode = 'dark'
+    page.window_focused = True
+    page.window_to_front()
 
     page.window_width = 900
     page.window_height = 600
@@ -17,21 +18,33 @@ def main(page: ft.Page) -> None:
         color_scheme_seed='#5a84ff'
     )
 
-    page.window_visible = False
-
     def window_event(e):
         if e.data == 'close':
             page.window_destroy()
 
+    # должен выводиться AlertDialog при выявленных несохраненных изменениях
+
     page.window_prevent_close = True
     page.on_window_event = window_event
+
+    page.padding = 0
+
+    # page.window_bgcolor = ft.colors.TRANSPARENT
+    # page.bgcolor = ft.colors.TRANSPARENT
+
+    main_container = Container(
+        expand=True,
+        content=Row(
+
+        )
+
+    )
+
+    page.add(main_container)
+    page.update()
 
 
 if __name__ == '__main__':
     abc = ft.app(target=main,
                  assets_dir='assets'
                  )
-    if not abc:
-        print(False)
-    else:
-        print(True)
