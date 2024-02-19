4fc366b08c98da1fe60a4f108040cdee1499672b
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-19 18:36:29 +1000

    added a simple navigation rail

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from flet_core.control import Control
 
 
 def main(page: ft.Page) -> None:
-    page.title = 'Settings'
+    page.title = 'Main menu'
     page.theme_mode = 'dark'
     page.window_focused = True
     page.window_to_front()
@@ -32,14 +32,53 @@ def main(page: ft.Page) -> None:
     # page.window_bgcolor = ft.colors.TRANSPARENT
     # page.bgcolor = ft.colors.TRANSPARENT
 
+    rail = ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        min_width=150,
+        min_extended_width=300,
+        leading=ft.FloatingActionButton(
+            icon=ft.icons.ADD,
+            text='Create'
+        ),
+        group_alignment=-1,
+        destinations=[
+            ft.NavigationRailDestination(
+                icon=ft.icons.EDIT_OUTLINED,
+                selected_icon=ft.icons.EDIT,
+                label='Edit'
+            ),
+
+            ft.NavigationRailDestination(
+                icon=ft.icons.SETTINGS_OUTLINED,
+                selected_icon=ft.icons.SETTINGS,
+                label='Settings'
+            ),
+
+            ft.NavigationRailDestination(
+                icon=ft.icons.INFO_OUTLINED,
+                selected_icon=ft.icons.INFO,
+                label='Info'
+            )
+
+        ]
+    )
+
+
+
     main_container = Container(
         expand=True,
         content=Row(
+            controls=[
+                rail,
+                ft.VerticalDivider(width=1),
 
+            ]
         )
 
     )
 
+    # ВАЖНО ПЕРЕДЕЛАТЬ ВСЁ, ЧТО РАНЬШЕ БЫЛО SETTINGS, В MAIN_MENU В settings.py & widget.py
     page.add(main_container)
     page.update()
 
