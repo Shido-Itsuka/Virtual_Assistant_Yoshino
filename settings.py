2b6bbf5a70c39d212bbfa4fabc9ea8d5c9af9e95
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-27 16:09:02 +1000

    added dropdown into alertdialog

--- settings.py
+++ settings.py
@@ -52,13 +52,49 @@ def main(page: ft.Page) -> None:
         creating_dialog.open = False
         page.update()
 
+    def pick_file_result(e: ft.FilePickerResultEvent):
+        if e.file:
+            selected_file = e.file
+        else:
+            selected_file = 'Cancelled'
+
+    pick_file_dialog = ft.FilePicker(on_result=pick_file_result)
+    selected_file = ''
+
+    def on_change_type_select(e):
+        if e.control.value == 'exec':
+            print(e.control.value)
+            command_type_child.content = ft.OutlinedButton(
+                "Выбрать .exe файл",
+                icon=ft.icons.UPLOAD_FILE,
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(
+                        radius=10
+                    )
+                ),
+                scale=1.1,
+                on_click=lambda _: pick_files(
+                    allow_multiple=False,
+
+                ),
+            )
+            page.update()
+        elif e.control.value == 'openweb':
+            print(e.control.value)
+        elif e.control.value == 'websearch':
+            print(e.control.value)
+        elif e.control.value == 'cmd':
+            print(e.control.value)
+        elif e.control.value == 'python':
+            print(e.control.value)
+
     rail = ft.NavigationRail(
         selected_index=0,
         label_type=ft.NavigationRailLabelType.SELECTED,
         height=500,
         width=130,
         min_width=130,
-        min_extended_width=150,
+        min_extended_width=200,
         bgcolor=ft.colors.TRANSPARENT,
         on_change=change_page,
         leading=Column(
@@ -68,7 +104,10 @@ def main(page: ft.Page) -> None:
                 ft.FloatingActionButton(
                     icon=ft.icons.ADD,
                     text='Create',
-                    on_click=open_create_dialog
+                    on_click=open_create_dialog,
+                    shape=ft.RoundedRectangleBorder(
+                        radius=10
+                    )
                 ),
 
             ]
@@ -82,7 +121,7 @@ def main(page: ft.Page) -> None:
             ft.NavigationRailDestination(
                 icon_content=ft.Icon(
                     name=ft.icons.EDIT_OUTLINED,
-                    color=ft.colors.WHITE70
+                    color=ft.colors.WHITE38,
                 ),
                 selected_icon_content=ft.Icon(
                     name=ft.icons.EDIT,
@@ -96,7 +135,7 @@ def main(page: ft.Page) -> None:
             ft.NavigationRailDestination(
                 icon_content=ft.Icon(
                     name=ft.icons.SETTINGS_OUTLINED,
-                    color=ft.colors.WHITE70
+                    color=ft.colors.WHITE38
                 ),
                 selected_icon_content=ft.Icon(
                     name=ft.icons.SETTINGS,
@@ -109,7 +148,7 @@ def main(page: ft.Page) -> None:
             ft.NavigationRailDestination(
                 icon_content=ft.Icon(
                     name=ft.icons.INFO_OUTLINED,
-                    color=ft.colors.WHITE70
+                    color=ft.colors.WHITE38
                 ),
                 selected_icon_content=ft.Icon(
                     name=ft.icons.INFO,
@@ -123,22 +162,55 @@ def main(page: ft.Page) -> None:
     )
 
     creating_dialog = ft.AlertDialog(
-        title=ft.Text('Create a new command!'),
-        content=Container(
-            bgcolor=ft.colors.BLUE_900,
-            width=600
+        title=Row(
+            controls=[
+                ft.Text('Create a new command!'),
+                ft.IconButton(ft.icons.CLOSE, on_click=close_create_dialog)
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
         ),
-        content_padding=10,
-        inset_padding=10,
-        actions_padding=10,
-        title_padding=10,
+        modal=True,
+        content=Container(
+            width=600,
+            border=ft.border.all(1, ft.colors.BLUE_900),
+            content=Column(
+                controls=[
+                    Row(
+                        controls=[
+                            type_select := ft.Dropdown(
+                                label='Типы команд',
+                                hint_text='Выберите тип команды',
+                                options=[
+                                    ft.dropdown.Option(key='exec', text='Запустить программу'),
+                                    ft.dropdown.Option(key='openweb', text='Открыть сайт'),
+                                    ft.dropdown.Option(key='websearch', text='Найти в браузере'),
+                                    ft.dropdown.Option(key='cmd', text='Выполнить команду в терминале'),
+                                    ft.dropdown.Option(key='python', text='Выполнить код на Python'),
+                                ],
+                                width=300,
+                                border_radius=ft.border_radius.all(5),
+                                on_change=on_change_type_select
+                            ),
+                            command_type_child := Container(
+                                content=ft.TextField(
+                                    disabled=True,
+                                    label='Ожидает выбора типа команды',
+                                )
+                            )
+                        ],
+
+                    )
+                ]
+            )
+        ),
+        content_padding=ft.padding.only(15, 0, 15, 0),
+        inset_padding=15,
+        actions_padding=15,
+        title_padding=15,
         actions=[
             ft.TextButton('Create',
                           on_click=close_create_dialog
                           ),
-            ft.TextButton('Cancel',
-                          on_click=close_create_dialog
-                          )
         ],
         actions_alignment=ft.MainAxisAlignment.END,
         shape=ft.RoundedRectangleBorder(radius=10),
@@ -220,7 +292,8 @@ def main(page: ft.Page) -> None:
                 ),
                 main_body
             ]
-        )
+        ),
+        blur=ft.Blur(10, 10, ft.BlurTileMode.MIRROR)
 
     )
 
