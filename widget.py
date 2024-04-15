e7c30858d42981f68f229697949369208fb0a8f3
Shido_Itsuka <trusoff.maksim2017@gmail.com>
2024-04-16 09:39:13 +1000

    temporary fix problem with settings' run

--- widget.py
+++ widget.py
@@ -3,83 +3,96 @@ from flet import Column, Row, Container, TextField, Checkbox
 from flet_core.control import Control
 import settings
 
+#
+# from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
+# import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
+# import wave  # создание и чтение аудиофайлов формата wav
+# import json  # работа с json-файлами и json-строками
+# import os  # работа с файловой системой
+#
+#
+# def record_and_recognize_audio(*args: tuple):
+#     """
+#     Запись и распознавание аудио
+#     """
+#     with microphone:
+#         recognized_data = ""
+#
+#         # регулирование уровня окружающего шума
+#         recognizer.adjust_for_ambient_noise(microphone, duration=2)
+#
+#         try:
+#             print("Listening...")
+#             audio = recognizer.listen(microphone, 5, 5)
+#
+#             with open("microphone-results.wav", "wb") as file:
+#                 file.write(audio.get_wav_data())
+#
+#         except speech_recognition.WaitTimeoutError:
+#             print("Can you check if your microphone is on, please?")
+#             return
+#
+#         # использование online-распознавания через Google
+#         try:
+#             print("Started recognition...")
+#             recognized_data = recognizer.recognize_google_cloud(audio, language="ru").lower()
+#
+#         except speech_recognition.UnknownValueError:
+#             pass
+#
+#         # в случае проблем с доступом в Интернет происходит попытка
+#         # использовать offline-распознавание через Vosk
+#         except speech_recognition.RequestError:
+#             print("Trying to use offline recognition...")
+#             recognized_data = use_offline_recognition()
+#
+#         return recognized_data
+#
+#
+# def use_offline_recognition():
+#     """
+#     Переключение на оффлайн-распознавание речи
+#     :return: распознанная фраза
+#     """
+#     recognized_data = ""
+#     try:
+#         # проверка наличия модели на нужном языке в каталоге приложения
+#         if not os.path.exists("models/vosk-model-small-ru-0.22"):
+#             print("Please download the model from:\n"
+#                   "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+#             exit(1)
+#
+#         # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
+#         wave_audio_file = wave.open("microphone-results.wav", "rb")
+#         model = Model("models/vosk-model-small-ru-0.4")
+#         offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+#
+#         data = wave_audio_file.readframes(wave_audio_file.getnframes())
+#         if len(data) > 0:
+#             if offline_recognizer.AcceptWaveform(data):
+#                 recognized_data = offline_recognizer.Result()
+#
+#                 # получение данных распознанного текста из JSON-строки
+#                 # (чтобы можно было выдать по ней ответ)
+#                 recognized_data = json.loads(recognized_data)
+#                 recognized_data = recognized_data["text"]
+#     except:
+#         print("Sorry, speech service is unavailable. Try again later")
+#
+#     return recognized_data
 
-from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
-import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
-import wave  # создание и чтение аудиофайлов формата wav
-import json  # работа с json-файлами и json-строками
-import os  # работа с файловой системой
-
-
-def record_and_recognize_audio(*args: tuple):
-    """
-    Запись и распознавание аудио
-    """
-    with microphone:
-        recognized_data = ""
-
-        # регулирование уровня окружающего шума
-        recognizer.adjust_for_ambient_noise(microphone, duration=2)
-
-        try:
-            print("Listening...")
-            audio = recognizer.listen(microphone, 5, 5)
-
-            with open("microphone-results.wav", "wb") as file:
-                file.write(audio.get_wav_data())
-
-        except speech_recognition.WaitTimeoutError:
-            print("Can you check if your microphone is on, please?")
-            return
-
-        # использование online-распознавания через Google
-        try:
-            print("Started recognition...")
-            recognized_data = recognizer.recognize_google_cloud(audio, language="ru").lower()
-
-        except speech_recognition.UnknownValueError:
-            pass
-
-        # в случае проблем с доступом в Интернет происходит попытка
-        # использовать offline-распознавание через Vosk
-        except speech_recognition.RequestError:
-            print("Trying to use offline recognition...")
-            recognized_data = use_offline_recognition()
-
-        return recognized_data
-
-
-def use_offline_recognition():
-    """
-    Переключение на оффлайн-распознавание речи
-    :return: распознанная фраза
-    """
-    recognized_data = ""
-    try:
-        # проверка наличия модели на нужном языке в каталоге приложения
-        if not os.path.exists("models/vosk-model-small-ru-0.22"):
-            print("Please download the model from:\n"
-                  "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-            exit(1)
-
-        # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
-        wave_audio_file = wave.open("microphone-results.wav", "rb")
-        model = Model("models/vosk-model-small-ru-0.4")
-        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
-
-        data = wave_audio_file.readframes(wave_audio_file.getnframes())
-        if len(data) > 0:
-            if offline_recognizer.AcceptWaveform(data):
-                recognized_data = offline_recognizer.Result()
-
-                # получение данных распознанного текста из JSON-строки
-                # (чтобы можно было выдать по ней ответ)
-                recognized_data = json.loads(recognized_data)
-                recognized_data = recognized_data["text"]
-    except:
-        print("Sorry, speech service is unavailable. Try again later")
-
-    return recognized_data
+# def execute_command_with_name(command_name: str, *args: list):
+#     """
+#     Выполнение заданной пользователем команды с дополнительными аргументами
+#     :param command_name: название команды
+#     :param args: аргументы, которые будут переданы в функцию
+#     :return:
+#     """
+#     for key in commands.keys():
+#         if command_name in key:
+#             commands[key](*args)
+#         else:
+#             pass  # print("Command not found")
 
 
 def main(page: ft.Page) -> None:
@@ -179,18 +192,17 @@ def main(page: ft.Page) -> None:
         page.update()
 
     def create_settings_window():
-        settings_button.data = True
+        # settings_button.data = True
         settings_button.update()
-        settings_app = ft.app(target=settings.main,
-                              assets_dir='assets'
-                              )
+        # page.visible = False
+        __import__('subprocess').call('python settings.py')
         print('test')
-        if not settings_app:
-            settings_button.data = False
-            settings_button.update()
-            print(f'settings window closed')
-        else:
-            print(True)
+        # if not settings_app:
+        #     settings_button.data = False
+        #     settings_button.update()
+        #     print(f'settings window closed')
+        # else:
+        #     print(True)
 
     def on_click_settings(e):
         print('settings button clicked!')
@@ -291,12 +303,18 @@ if __name__ == '__main__':
     ft.app(target=main,
            assets_dir='assets'
            )
-    recognizer = speech_recognition.Recognizer()
-    microphone = speech_recognition.Microphone()
-
-    while True:
-        # старт записи речи с последующим выводом распознанной речи
-        # и удалением записанного в микрофон аудио
-        voice_input = record_and_recognize_audio()
-        os.remove("microphone-results.wav")
-        print(voice_input)
+    # recognizer = speech_recognition.Recognizer()
+    # microphone = speech_recognition.Microphone()
+    #
+    # while True:
+    #     # старт записи речи с последующим выводом распознанной речи
+    #     # и удалением записанного в микрофон аудио
+    #     voice_input = record_and_recognize_audio()
+    #     os.remove("microphone-results.wav")
+    #     print(voice_input)
+    #
+    #     # отделение команд от дополнительной информации (аргументов)
+    #     voice_input = voice_input.split(" ")
+    #     command = voice_input[0]
+    #     command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
+    #     # execute_command_with_name(command, command_options)
